61def2963c47d9eb3b41c6cc52129ff75f42a0bb
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2019-08-19 20:16:38 +0300

    add solutions for 2018-08-15 and 2018-08-18

---
+++ 2018-08-15/2018-08-15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _2018_08_15
+{
+    /*
+        Given a multiset of integers, return whether it can be partitioned into two subsets whose sums are the same.
+        For example, given the multiset {15, 5, 20, 10, 35, 15, 10}, it would return true, since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55.
+        Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't split it up into two subsets that add up to the same sum.
+         */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] { 15, 5, 20, 10, 35, 15, 10 };
+            Console.WriteLine(CanDevideEqually(array));
+            Console.ReadKey();
+        }
+
+        static bool CanDevideEqually(int[] array)
+        {
+            var total = array.Sum();
+            if (total % 2 == 1) return false;
+            return CanSum(array, total / 2, 0);
+        }
+
+        static bool CanSum(int[] array, int sum, int startIndex)
+        {
+            if (sum == 0) return true;
+            if (startIndex == array.Length) return false;
+            return CanSum(array, sum, startIndex + 1) || CanSum(array, sum - array[startIndex], startIndex + 1);
+        }
+    }
+}
---
+++ 2018-08-18/2018-08-18/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _2018_08_18
+{
+    /*
+        Given a 2D matrix of characters and a target word, write a function that returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+        For example, given the following matrix:
+        [['F', 'A', 'C', 'I'],
+         ['O', 'B', 'Q', 'P'],
+         ['A', 'N', 'O', 'B'],
+         ['M', 'A', 'S', 'S']]
+        and the target word 'FOAM', you should return true, since it's the leftmost column. Similarly, given the target word 'MASS', you should return true, since it's the last row.   
+        
+        Assuming matrix is square
+        and matrix edge length is equal to target word length.
+
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] matrix = new[,] { 
+                { 'F', 'A', 'C', 'I' }, 
+                { 'O', 'B', 'Q', 'P' }, 
+                { 'A', 'N', 'O', 'B' }, 
+                { 'M', 'A', 'S', 'S' }
+            };
+
+            Console.WriteLine(FindWord(matrix, "MASS"));
+            Console.ReadKey();
+        }
+
+        static bool FindWord(char[,] matrix, string word)
+        {
+            if (word == null) throw new ArgumentNullException(nameof(word));
+            if (matrix == null) throw new ArgumentNullException(nameof(matrix));
+            if (matrix.Rank != 2) throw new ArgumentException("matrix should have 2 dimentions");
+            if (matrix.GetLength(0) != word.Length) throw new ArgumentException("matrix dimention 0 does not match the length of word");
+            if (matrix.GetLength(1) != word.Length) throw new ArgumentException("matrix dimention 1 does not match the length of word");
+
+            char[] wordChars = word.ToCharArray();
+            for(var i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (CheckLine(matrix, wordChars, true, i)) return true;
+            }
+
+            for (var i = 0; i < matrix.GetLength(1); i++)
+            {
+                if (CheckLine(matrix, wordChars, false, i)) return true;
+            }
+
+            return false;
+        }
+
+        static bool CheckLine(char[,] matrix, char[] wordChars, bool vertical, int shift)
+        {
+            for(var i = 0; i < wordChars.Length; i++)
+            {
+                if ((vertical ? matrix[shift, i] : matrix[i, shift]) != wordChars[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
