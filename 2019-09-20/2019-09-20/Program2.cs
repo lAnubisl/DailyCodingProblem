7ec450263a1c8e158c827f45f23eafcb6f53b629
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2019-09-21 10:53:31 +0300

    add better solutions

---
+++ 2019-09-20/2019-09-20/Program2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2019_09_20
+{    
+    /*
+     Given a number in the form of a list of digits, return all possible permutations.
+     For example, given [1,2,3], return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    */
+    class Program2
+    {
+        private static IEnumerable<int[]> Permute(int[] a, int i = 0)
+        {
+            if (i == a.Length - 1)
+            {
+                yield return a;
+            }
+
+            for (int j = i; j <= a.Length - 1; j++)
+            {
+                Swap(a, i, j);
+                foreach (var p in Permute(a, i + 1)) yield return p;
+                Swap(a, i, j);
+            }
+        }
+
+        public static void Swap(int[] a, int i, int j)
+        {
+            var t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+
+        public static void Main()
+        {
+            var a = new int[] { 1, 2, 3 };
+            foreach (var permutation in Permute(a))
+            Console.WriteLine($"[{string.Join(", ", permutation)}]");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2019-09-20/2019-09-20/Program3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2019_09_20
+{
+    /*
+     Given a number in the form of a list of digits, return all possible permutations.
+     For example, given [1,2,3], return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    */
+    class Program3
+    {
+        public static void Main()
+        {
+            var a = new int[] { 1, 2, 3 };
+            foreach (var permutation in Permutations(a))
+                Console.WriteLine($"[{string.Join(", ", permutation)}]");
+            Console.ReadLine();
+        }
+        /*
+         1  2  3
+         -------
+         {2 1} 3
+         2 {3 1}
+         {3 2} 1
+         3 {1 2}
+         {1 3} 2
+         1 {2 3}
+       */
+        static IEnumerable<int[]> Permutations(int[] a)
+        {
+            var count = Fuctorial(a.Length);
+            var i = 0;
+            while (count > 0)
+            {
+                Swap(a, i, i + 1);
+                yield return a;
+                count--;
+                i = i == a.Length - 2 ? 0 : i + 1; // a.Length - 2 because the last index is a.Length - 1 and we should not go so far because we are swapping current index with next one
+            }
+        }
+
+        static void Swap(int[] a, int i, int j)
+        {
+            var t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+
+        static int Fuctorial(int n)
+        {
+            return n == 0 ? 1 : n * Fuctorial(n - 1);
+        }
+    }
+}
